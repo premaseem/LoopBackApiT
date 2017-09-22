8a5c6b34821aa37b5d03ec7e2c6c9a1cc79aed8f
Aseem Jain <premaseem.webmaster@gmail.com>
2017-09-22 13:45:12 -0500

    Important - has many relation implementation for drvier notes

--- common/models/note.js
+++ common/models/note.js
@@ -1,5 +1,43 @@
 'use strict';
 
-module.exports = function (Notes) {
+var app = require('../../server/server');
 
+module.exports = function(Notes) {
+  Notes.afterRemote('create', function(context, note, next) {
+    var noteReq = context.req.body;
+    if (noteReq && !noteReq.driverId){
+      next(new Error("Note should always belong to a Company. Please specify."));
+      return;
+    }
+
+    app.models.DriverNoteTieup.create({
+      driverId: noteReq.driverId,
+      noteId: note.id
+    }, function(err, result){
+      if (err){
+        next(err);
+        return;
+      }
+      next(null, result);
+    });
+
+  });
+
+  Notes.beforeRemote('replaceById', function(context, note, next) {
+    var noteReq = context.req.body;
+    console.log("Chcking noteReq", noteReq);
+
+    app.models.DriverNoteTieup.upsertWithWhere({
+      noteId: noteReq.id
+    },{
+      driverId: noteReq.driverId,
+      noteId: noteReq.id
+    }, function(err, results){
+      if (err){
+        next(err);
+        return;
+      }
+      next();
+    });
+  });
 };
