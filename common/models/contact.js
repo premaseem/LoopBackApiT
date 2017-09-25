6749402974282feb1c0d3cf2de7164e9f19f3ff3
Aseem Jain <premaseem.webmaster@gmail.com>
2017-09-25 12:25:07 -0500

    implemented driver has many contacts relation ship

--- common/models/contact.js
+++ common/models/contact.js
@@ -1,5 +1,43 @@
 'use strict';
 
-module.exports = function (Contact) {
+var app = require('../../server/server');
 
+module.exports = function (Contact) {
+  Contact.afterRemote('create', function(context, note, next) {
+    var req = context.req.body;
+    if (req && !req.driverId){
+      next(new Error("contact should always belong to a Driver. Please specify."));
+      return;
+    }
+
+    app.models.DriverContactTieup.create({
+      driverId: req.driverId,
+      contactId: contact.id
+    }, function(err, result){
+      if (err){
+        next(err);
+        return;
+      }
+      next(null, result);
+    });
+
+  });
+
+  Contact.beforeRemote('replaceById', function(context, note, next) {
+    var req = context.req.body;
+    console.log("Chcking req", req);
+
+    app.models.DriverContactTieup.upsertWithWhere({
+      noteId: req.id
+    },{
+      driverId: req.driverId,
+      contactId: req.id
+    }, function(err, results){
+      if (err){
+        next(err);
+        return;
+      }
+      next();
+    });
+  });
 };
