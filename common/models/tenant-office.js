b5760c780a74f575422972eaf52404bf045ec228
Aseem Jain <premaseem.webmaster@gmail.com>
2017-09-27 10:43:50 -0500

    has many relation between Tenant and TenantOffice

--- common/models/tenant-office.js
+++ common/models/tenant-office.js
@@ -1,5 +1,46 @@
 'use strict';
 
+var app = require('../../server/server');
+
 module.exports = function(Tenantoffice) {
+  Tenantoffice.afterRemote('create', function(context, tenantoffice, next) {
+    var req = context.req.body;
+    if (req && !req.tenantId){
+      next(new Error("Tenantoffice should always belong to a tenant. Please specify."));
+      return;
+    }
+
+    app.models.TenantTenantOfficeTieup.create({
+      tenantId: req.tenantId,
+      tenantofficeId: tenantoffice.id
+    }, function(err, result){
+      if (err){
+        next(err);
+        return;
+      }
+      next(null, result);
+    });
+
+  });
+
+  Tenantoffice.beforeRemote('replaceById', function(context, tenantoffice, next) {
+    var req = context.req.body;
+    console.log("Chcking req", req);
+
+    app.models.TenantTenantOfficeTieup.upsertWithWhere({
+      tenantofficeId: req.id
+    },{
+      tenantId: req.tenantId,
+      tenantofficeId: req.id
+    }, function(err, results){
+      if (err){
+        next(err);
+        return;
+      }
+      next();
+    });
+  });
+
+
 
 };
